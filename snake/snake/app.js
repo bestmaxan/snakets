684a996f7c27f4fe92d33b880923c212650f978d
maximan <maximan@MAX-PC>
2016-10-03 17:23:45 -0400

    draw method added

--- snake/snake/app.js
+++ snake/snake/app.js
@@ -1,23 +1,60 @@
-var Greeter = (function () {
-    function Greeter(element) {
-        this.element = element;
-        this.element.innerHTML += "The g time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
-    }
-    Greeter.prototype.start = function () {
-        var _this = this;
-        this.timerToken = setInterval(function () { return _this.span.innerHTML = new Date().toUTCString(); }, 500);
-    };
-    Greeter.prototype.stop = function () {
-        clearTimeout(this.timerToken);
-    };
-    return Greeter;
-}());
 window.onload = function () {
     var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
+    var s = new Program(el);
+    s.init();
 };
+var DrawElement;
+(function (DrawElement) {
+    DrawElement[DrawElement["head"] = 0] = "head";
+    DrawElement[DrawElement["body"] = 1] = "body";
+    DrawElement[DrawElement["tail"] = 2] = "tail";
+    DrawElement[DrawElement["fruit"] = 3] = "fruit";
+})(DrawElement || (DrawElement = {}));
+var Program = (function () {
+    function Program(element) {
+        this.element = element;
+        this.element.innerHTML = "";
+        for (var i = 0; i < 400; i++) {
+            this.div = document.createElement('div');
+            this.div.className = "cell";
+            //this.div.innerHTML = i.toString();
+            this.element.appendChild(this.div);
+        }
+    }
+    Program.prototype.init = function () {
+        var p1 = new Point(this.element);
+        p1.x = 4;
+        p1.y = 7;
+        p1.sym = DrawElement.fruit;
+        p1.Draw();
+    };
+    return Program;
+}());
+var Point = (function () {
+    function Point(element) {
+        this.element = element;
+    }
+    Point.prototype.Draw = function () {
+        var cls = "";
+        switch (this.sym) {
+            case DrawElement.head:
+                cls = " sHd";
+                break;
+            case DrawElement.body:
+                cls = " sBd";
+                break;
+            case DrawElement.tail:
+                cls = " sBd";
+                break;
+            case DrawElement.fruit:
+                cls = " frt";
+                break;
+        }
+        this.element.getElementsByClassName("cell")[this.convertCoordinates(this.x, this.y)].className += cls;
+    };
+    Point.prototype.convertCoordinates = function (x, y) {
+        return 20 * (x - 1) + (y - 1);
+    };
+    return Point;
+}());
 //# sourceMappingURL=app.js.map
--- snake/snake/app.ts
+++ snake/snake/app.ts
@@ -1,28 +1,71 @@
-﻿class Greeter {
-    element: HTMLElement;
-    span: HTMLElement;
-    timerToken: number;
+﻿window.onload = () => {
+    var el = document.getElementById('content');
+    var s = new Program(el);
+    s.init();
+};
+
+enum DrawElement {
+    head,
+    body,
+    tail,
+    fruit
+}
+
+class Program {
+    element: HTMLElement; 
+    div: HTMLElement; 
 
     constructor(element: HTMLElement) {
         this.element = element;
-        this.element.innerHTML += "The g time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
+        this.element.innerHTML = "";
+        for (var i = 0; i < 400; i++) {
+            this.div = document.createElement('div');
+            this.div.className = "cell";
+            //this.div.innerHTML = i.toString();
+            this.element.appendChild(this.div);
+        }
     }
 
-    start() {
-        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
+    init() {
+        var p1 = new Point(this.element);
+        p1.x = 4;
+        p1.y = 7;
+        p1.sym = DrawElement.fruit;
+        p1.Draw();
     }
-
-    stop() {
-        clearTimeout(this.timerToken);
-    }
-
 }
 
-window.onload = () => {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
-};
+class Point {
+    x: number;
+    y: number;
+    sym: DrawElement;
+    element: HTMLElement; 
+
+    constructor(element: HTMLElement) {
+        this.element = element;
+    }
+
+    public Draw() {
+        let cls: string = "";
+        switch (this.sym) {
+            case DrawElement.head:
+                cls = " sHd";
+                break;
+            case DrawElement.body:
+                cls = " sBd";
+                break;
+            case DrawElement.tail:
+                cls = " sBd";
+                break;
+            case DrawElement.fruit:
+                cls = " frt";
+                break;
+
+        }
+        this. element.getElementsByClassName("cell")[this.convertCoordinates(this.x, this.y)].className += cls;
+    }
+
+    private convertCoordinates(x: number, y: number): number {
+        return 20 * (x - 1) + (y - 1);
+    }
+}
