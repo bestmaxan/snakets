64cf3c8bb5b2fe405daeb1264ff18f53bcf841c8
maximan <maximan@MAX-PC>
2016-10-05 18:10:10 -0400

    beta

--- snake/snake/app.js
+++ snake/snake/app.js
@@ -1,60 +1,264 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
 window.onload = function () {
     var el = document.getElementById('content');
-    var s = new Program(el);
+    var s = new Sn.Program(el);
     s.init();
 };
-var DrawElement;
-(function (DrawElement) {
-    DrawElement[DrawElement["head"] = 0] = "head";
-    DrawElement[DrawElement["body"] = 1] = "body";
-    DrawElement[DrawElement["tail"] = 2] = "tail";
-    DrawElement[DrawElement["fruit"] = 3] = "fruit";
-})(DrawElement || (DrawElement = {}));
-var Program = (function () {
-    function Program(element) {
-        this.element = element;
-        this.element.innerHTML = "";
-        for (var i = 0; i < 400; i++) {
-            this.div = document.createElement('div');
-            this.div.className = "cell";
-            //this.div.innerHTML = i.toString();
-            this.element.appendChild(this.div);
+var Sn;
+(function (Sn) {
+    var element;
+    var DrawElement;
+    (function (DrawElement) {
+        DrawElement[DrawElement["head"] = 0] = "head";
+        DrawElement[DrawElement["body"] = 1] = "body";
+        DrawElement[DrawElement["wall"] = 2] = "wall";
+        DrawElement[DrawElement["fruit"] = 3] = "fruit";
+    })(DrawElement || (DrawElement = {}));
+    var Direction;
+    (function (Direction) {
+        Direction[Direction["Left"] = 0] = "Left";
+        Direction[Direction["Right"] = 1] = "Right";
+        Direction[Direction["Up"] = 2] = "Up";
+        Direction[Direction["Down"] = 3] = "Down";
+    })(Direction || (Direction = {}));
+    var Program = (function () {
+        function Program(_element) {
+            element = _element;
+            element.innerHTML = "";
+            for (var i = 0; i < 400; i++) {
+                var div = document.createElement('div');
+                div.className = "cell";
+                element.appendChild(div);
+            }
         }
-    }
-    Program.prototype.init = function () {
-        var p1 = new Point(this.element);
-        p1.x = 4;
-        p1.y = 7;
-        p1.sym = DrawElement.fruit;
-        p1.Draw();
-    };
-    return Program;
-}());
-var Point = (function () {
-    function Point(element) {
-        this.element = element;
-    }
-    Point.prototype.Draw = function () {
-        var cls = "";
-        switch (this.sym) {
-            case DrawElement.head:
-                cls = " sHd";
-                break;
-            case DrawElement.body:
-                cls = " sBd";
-                break;
-            case DrawElement.tail:
-                cls = " sBd";
-                break;
-            case DrawElement.fruit:
-                cls = " frt";
-                break;
+        Program.prototype.init = function () {
+            var p1 = new Point(4, 5, DrawElement.body);
+            var sn = new Snake(p1, 4, Direction.Right);
+            sn.Draw();
+            var foodCr = new FoodCreator(20, 20);
+            var food = foodCr.Create();
+            food.Draw();
+            document.onkeydown = function (e) {
+                sn.HandleKey(e.keyCode);
+            };
+            var goSnake = setInterval(function () {
+                if (sn.IsHitTail() || sn.IsHitWall()) {
+                    clearInterval(goSnake);
+                    food.Clear();
+                    sn.Clear();
+                    var sm1 = new VLine(4, 11, 8);
+                    var sm2 = new VLine(4, 11, 13);
+                    var sm3 = new HLine(7, 14, 15);
+                    var sm4 = new Figure();
+                    sm4.pList.push(new Point(6, 16, DrawElement.wall));
+                    sm4.pList.push(new Point(5, 17, DrawElement.wall));
+                    sm4.pList.push(new Point(15, 16, DrawElement.wall));
+                    sm4.pList.push(new Point(16, 17, DrawElement.wall));
+                    sm1.Draw();
+                    sm2.Draw();
+                    sm3.Draw();
+                    sm4.Draw();
+                }
+                else {
+                    if (sn.Eat(food)) {
+                        food = foodCr.Create();
+                        food.Draw();
+                    }
+                    sn.Move();
+                }
+            }, 150);
+        };
+        return Program;
+    }());
+    Sn.Program = Program;
+    var Point = (function () {
+        function Point(_x, _y, _sym) {
+            if (typeof _x === "object") {
+                this.x = _x.x;
+                this.y = _x.y;
+                this.sym = _x._sym;
+            }
+            else {
+                this.x = _x;
+                this.y = _y;
+                this.sym = _sym;
+            }
         }
-        this.element.getElementsByClassName("cell")[this.convertCoordinates(this.x, this.y)].className += cls;
-    };
-    Point.prototype.convertCoordinates = function (x, y) {
-        return 20 * (x - 1) + (y - 1);
-    };
-    return Point;
-}());
+        Object.defineProperty(Point.prototype, "sym", {
+            get: function () {
+                return this._sym;
+            },
+            set: function (sym) {
+                this._sym = sym;
+                this.cls = this.getCssClass(sym);
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Point.prototype.Move = function (offset, dirctn) {
+            if (dirctn == Direction.Right) {
+                this.x = this.x + offset;
+            }
+            else if (dirctn == Direction.Left) {
+                this.x = this.x - offset;
+            }
+            else if (dirctn == Direction.Up) {
+                this.y = this.y - offset;
+            }
+            else if (dirctn == Direction.Down) {
+                this.y = this.y + offset;
+            }
+        };
+        Point.prototype.IsHit = function (p) {
+            return (this.x == p.x && this.y == p.y);
+        };
+        Point.prototype.getCssClass = function (sym) {
+            var cls = "";
+            switch (sym) {
+                case DrawElement.head:
+                    cls = " sHd";
+                    break;
+                case DrawElement.body:
+                    cls = " sBd";
+                    break;
+                case DrawElement.wall:
+                    cls = " wall";
+                    break;
+                case DrawElement.fruit:
+                    cls = " frt";
+                    break;
+            }
+            return cls;
+        };
+        Point.prototype.Draw = function () {
+            element.getElementsByClassName("cell")[this.convertCoordinates(this.x, this.y)].className += this.cls;
+        };
+        Point.prototype.Clear = function (clss) {
+            element.getElementsByClassName("cell")[this.convertCoordinates(this.x, this.y)].className =
+                element.getElementsByClassName("cell")[this.convertCoordinates(this.x, this.y)].className.replace(clss || this.cls, "");
+        };
+        Point.prototype.convertCoordinates = function (x, y) {
+            return 20 * (y - 1) + (x - 1);
+        };
+        return Point;
+    }());
+    var Figure = (function () {
+        function Figure() {
+            this.pList = new Array();
+        }
+        Figure.prototype.Draw = function () {
+            for (var ind in this.pList) {
+                this.pList[ind].Draw();
+            }
+        };
+        return Figure;
+    }());
+    var Snake = (function (_super) {
+        __extends(Snake, _super);
+        function Snake(tail, len, dirctn) {
+            _super.call(this);
+            this.direction = dirctn;
+            for (var i = 0; i < len; i++) {
+                var p = new Point(tail);
+                p.Move(i, this.direction);
+                this.pList.push(p);
+            }
+        }
+        Snake.prototype.Move = function () {
+            var tail = this.pList[0];
+            this.pList.splice(0, 1);
+            var head = this.GetNextPoint();
+            this.pList.push(head);
+            tail.Clear();
+            head.Draw();
+        };
+        Snake.prototype.GetNextPoint = function () {
+            var head = this.pList[this.pList.length - 1];
+            var nPoint = new Point(head);
+            nPoint.Move(1, this.direction);
+            return nPoint;
+        };
+        Snake.prototype.HandleKey = function (key) {
+            if (key == 37 && this.direction != Direction.Right) {
+                this.direction = Direction.Left;
+            }
+            else if (key == 38 && this.direction != Direction.Down) {
+                this.direction = Direction.Up;
+            }
+            else if (key == 39 && this.direction != Direction.Left) {
+                this.direction = Direction.Right;
+            }
+            else if (key == 40 && this.direction != Direction.Up) {
+                this.direction = Direction.Down;
+            }
+        };
+        Snake.prototype.Eat = function (food) {
+            var head = this.GetNextPoint();
+            if (head.IsHit(food)) {
+                food.sym = head.sym;
+                food.Clear(" frt");
+                food.Draw();
+                this.pList.push(food);
+                return true;
+            }
+            return false;
+        };
+        Snake.prototype.IsHitTail = function () {
+            var head = this.pList[this.pList.length - 1];
+            for (var i = 0; i < this.pList.length - 2; i++) {
+                if (head.IsHit(this.pList[i]))
+                    return true;
+            }
+            return false;
+        };
+        Snake.prototype.IsHitWall = function () {
+            var head = this.pList[this.pList.length - 1];
+            if (head.x > 19 || head.y > 19 || head.x < 2 || head.y < 2)
+                return true;
+            return false;
+        };
+        Snake.prototype.Clear = function () {
+            for (var ind in this.pList) {
+                this.pList[ind].Clear();
+            }
+        };
+        return Snake;
+    }(Figure));
+    var HLine = (function (_super) {
+        __extends(HLine, _super);
+        function HLine(xLeft, xRight, y) {
+            _super.call(this);
+            for (var x = xLeft; x <= xRight; x++) {
+                this.pList.push(new Point(x, y, DrawElement.wall));
+            }
+        }
+        return HLine;
+    }(Figure));
+    var VLine = (function (_super) {
+        __extends(VLine, _super);
+        function VLine(yTop, yBottom, x) {
+            _super.call(this);
+            for (var y = yTop; y <= yBottom; y++) {
+                this.pList.push(new Point(x, y, DrawElement.wall));
+            }
+        }
+        return VLine;
+    }(Figure));
+    var FoodCreator = (function () {
+        function FoodCreator(mapWidth, mapHeight) {
+            this.mapWidth = mapWidth;
+            this.mapHeight = mapHeight;
+        }
+        FoodCreator.prototype.Create = function () {
+            var x = Math.floor(Math.random() * this.mapWidth);
+            var y = Math.floor(Math.random() * this.mapHeight);
+            return new Point(x, y, DrawElement.fruit);
+        };
+        return FoodCreator;
+    }());
+})(Sn || (Sn = {}));
 //# sourceMappingURL=app.js.map
--- snake/snake/app.ts
+++ snake/snake/app.ts
@@ -1,71 +1,284 @@
 ﻿window.onload = () => {
     var el = document.getElementById('content');
-    var s = new Program(el);
+    var s = new Sn.Program(el);
     s.init();
+
+    
 };
 
-enum DrawElement {
-    head,
-    body,
-    tail,
-    fruit
-}
+namespace Sn {
+    let element: HTMLElement;
 
-class Program {
-    element: HTMLElement; 
-    div: HTMLElement; 
+    enum DrawElement {
+        head,
+        body,
+        wall,
+        fruit
+    }
 
-    constructor(element: HTMLElement) {
-        this.element = element;
-        this.element.innerHTML = "";
-        for (var i = 0; i < 400; i++) {
-            this.div = document.createElement('div');
-            this.div.className = "cell";
-            //this.div.innerHTML = i.toString();
-            this.element.appendChild(this.div);
+    enum Direction {
+        Left,
+        Right,
+        Up,
+        Down
+    }
+
+    export class Program {
+        constructor(_element: HTMLElement) {
+            element = _element;
+            element.innerHTML = "";
+            for (let i = 0; i < 400; i++) {
+                let div = document.createElement('div');
+                div.className = "cell";
+                element.appendChild(div);
+            }
+        }
+
+        init() {
+            var p1 = new Point(4, 5, DrawElement.body);
+            var sn = new Snake(p1, 4, Direction.Right);
+            sn.Draw();
+
+            var foodCr = new FoodCreator(20, 20);
+            var food = foodCr.Create();
+            food.Draw();
+
+            document.onkeydown = function (e) {
+                sn.HandleKey(e.keyCode);
+            }
+
+            var goSnake = setInterval(function () {
+                if (sn.IsHitTail() || sn.IsHitWall()) {
+                    clearInterval(goSnake);
+                    food.Clear();
+                    sn.Clear();
+                    let sm1 = new VLine(4, 11, 8);
+                    let sm2 = new VLine(4, 11, 13);
+                    let sm3 = new HLine(7, 14, 15);
+                    let sm4 = new Figure();
+                    sm4.pList.push(new Point(6, 16, DrawElement.wall));
+                    sm4.pList.push(new Point(5, 17, DrawElement.wall));
+                    sm4.pList.push(new Point(15, 16, DrawElement.wall));
+                    sm4.pList.push(new Point(16, 17, DrawElement.wall));
+                    sm1.Draw();
+                    sm2.Draw();
+                    sm3.Draw();
+                    sm4.Draw();
+                } else {
+                    if (sn.Eat(food)) {
+                        food = foodCr.Create();
+                        food.Draw();
+                    }
+                    sn.Move();
+                }
+            }, 150);
         }
     }
 
-    init() {
-        var p1 = new Point(this.element);
-        p1.x = 4;
-        p1.y = 7;
-        p1.sym = DrawElement.fruit;
-        p1.Draw();
-    }
-}
+    class Point {
+        x: number;
+        y: number;
+        private _sym: DrawElement;
+        cls: string;
 
-class Point {
-    x: number;
-    y: number;
-    sym: DrawElement;
-    element: HTMLElement; 
-
-    constructor(element: HTMLElement) {
-        this.element = element;
-    }
-
-    public Draw() {
-        let cls: string = "";
-        switch (this.sym) {
-            case DrawElement.head:
-                cls = " sHd";
-                break;
-            case DrawElement.body:
-                cls = " sBd";
-                break;
-            case DrawElement.tail:
-                cls = " sBd";
-                break;
-            case DrawElement.fruit:
-                cls = " frt";
-                break;
-
+        constructor(_x: Point);
+        constructor(_x: number, _y: number, _sym: DrawElement);
+                
+        constructor(_x: number | Point, _y?: number, _sym?: DrawElement) {
+            if (typeof _x === "object") {
+                this.x = _x.x;
+                this.y = _x.y;
+                this.sym = _x._sym;
+            } else {
+                this.x = _x;
+                this.y = _y;
+                this.sym = _sym;
+            }
         }
-        this. element.getElementsByClassName("cell")[this.convertCoordinates(this.x, this.y)].className += cls;
+
+        get sym() {
+            return this._sym;
+        }
+        set sym(sym: DrawElement) {
+            this._sym = sym;
+            this.cls = this.getCssClass(sym);
+        }
+
+        Move(offset: number, dirctn: Direction) {
+            if (dirctn == Direction.Right) {
+                this.x = this.x + offset;
+            } else if (dirctn == Direction.Left) {
+                this.x = this.x - offset;
+            } else if (dirctn == Direction.Up) {
+                this.y = this.y - offset;
+            } else if (dirctn == Direction.Down) {
+                this.y = this.y + offset;
+            }
+        }
+
+        IsHit(p: Point): boolean {
+            return (this.x == p.x && this.y == p.y);
+        }
+
+        private getCssClass(sym: DrawElement):string {
+            let cls: string = "";
+            switch (sym) {
+                case DrawElement.head:
+                    cls = " sHd";
+                    break;
+                case DrawElement.body:
+                    cls = " sBd";
+                    break;
+                case DrawElement.wall:
+                    cls = " wall";
+                    break;
+                case DrawElement.fruit:
+                    cls = " frt";
+                    break;
+            }
+            return cls;
+        }
+
+        Draw() {
+            element.getElementsByClassName("cell")[this.convertCoordinates(this.x, this.y)].className += this.cls;
+        }
+
+        Clear();
+        Clear(clss: string);
+        Clear(clss?: string)
+        {
+            element.getElementsByClassName("cell")[this.convertCoordinates(this.x, this.y)].className =
+                element.getElementsByClassName("cell")[this.convertCoordinates(this.x, this.y)].className.replace(clss||this.cls,"");
+        }
+
+        private convertCoordinates(x: number, y: number): number {
+            return 20 * (y - 1) + (x - 1);
+        }
     }
 
-    private convertCoordinates(x: number, y: number): number {
-        return 20 * (x - 1) + (y - 1);
+    class Figure {
+        pList: Array<Point>;
+
+        constructor() {
+            this.pList = new Array<Point>();
+        }
+
+        Draw() {
+            for (var ind in this.pList) {
+                this.pList[ind].Draw();
+            }
+        }
+    }
+
+    class Snake extends Figure {
+        direction: Direction;
+
+        constructor(tail: Point, len: number, dirctn: Direction) {
+            super();
+            this.direction = dirctn;
+            for (let i = 0; i < len; i++) {
+                var p = new Point(tail);
+                p.Move(i, this.direction);
+                this.pList.push(p);
+            }
+        }
+
+        Move() {
+            let tail = this.pList[0];
+            this.pList.splice(0, 1);
+            let head = this.GetNextPoint();
+            this.pList.push(head);
+
+            tail.Clear();
+            head.Draw();
+        }
+
+        GetNextPoint(): Point {
+            let head = this.pList[this.pList.length-1];
+            let nPoint = new Point(head);
+            nPoint.Move(1, this.direction);
+            return nPoint;
+        }
+
+        HandleKey(key: number) {
+            if (key == 37 && this.direction != Direction.Right) {
+                this.direction = Direction.Left;
+            } else if (key == 38 && this.direction != Direction.Down) {
+                this.direction = Direction.Up;
+            } else if (key == 39 && this.direction != Direction.Left) {
+                this.direction = Direction.Right;
+            } else if (key == 40 && this.direction != Direction.Up) {
+                this.direction = Direction.Down;
+            }
+        }
+
+        Eat(food: Point): boolean {
+            let head = this.GetNextPoint();
+            if (head.IsHit(food)) {                
+                food.sym = head.sym;
+                food.Clear(" frt");
+                food.Draw();
+                this.pList.push(food);
+                return true;
+            }
+            return false;
+        }
+
+        IsHitTail(): boolean {
+            let head = this.pList[this.pList.length - 1];
+            for (let i = 0; i < this.pList.length - 2; i++) {
+                if (head.IsHit(this.pList[i]))
+                    return true;
+            }
+            return false;
+        }
+
+        IsHitWall(): boolean {
+            let head = this.pList[this.pList.length - 1];
+            if (head.x > 19 || head.y > 19 || head.x < 2 || head.y < 2)
+                return true;
+            return false;
+        }
+
+        Clear() {
+            for (var ind in this.pList) {
+                this.pList[ind].Clear();
+            }
+        }
+    }
+
+    class HLine extends Figure {
+        constructor(xLeft: number, xRight: number, y: number) {
+            super();
+            for (var x = xLeft; x <= xRight; x++) {
+                this.pList.push(new Point(x, y, DrawElement.wall));
+            }
+            
+        }
+    }
+
+    class VLine extends Figure {
+        constructor(yTop: number, yBottom: number, x: number) {
+            super();
+            for (var y = yTop; y <= yBottom; y++) {
+                this.pList.push(new Point(x, y, DrawElement.wall));
+            }
+        }        
+    }
+
+    class FoodCreator {
+        mapWidth: number;
+        mapHeight: number;
+
+        constructor(mapWidth: number, mapHeight: number) {
+            this.mapWidth = mapWidth;
+            this.mapHeight = mapHeight;
+        }
+
+        Create(): Point {
+            let x = Math.floor(Math.random() * this.mapWidth);
+            let y = Math.floor(Math.random() * this.mapHeight);
+            return new Point(x, y, DrawElement.fruit);
+        }
     }
 }
